d2935caccbb3579f05fb431c9654147d525babef
cloudfox <38594598+cloudfox@users.noreply.github.com>
2018-04-21 15:10:07 -0700

    Initial commit

---
+++ Source/building/buildingGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "buildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_API AbuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
